9e0ec2ded1bfa04fc85dac68a05e7284438104e9
Volodymyr Pashchenkov <mironnn@gmail.com>
2015-03-12 21:00:57 +0200

    linux+sqlite

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ CSRF_ENABLED = True
 SECRET_KEY = '3453kjnfv2i24uhwivnweowe3n4'
 
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-# if os.environ.get('DATABASE_URL') is None:
-#     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-# else:
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+if os.environ.get('DATABASE_URL') is None:
+  SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+  SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
